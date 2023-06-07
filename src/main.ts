6f90fe9184af0130ce4db271f2c79640bb60ed72
Ethan Muller <hi@emu.media>
2023-06-07 15:20:54 -0400

    fix button? i dunno

--- src/main.ts
+++ src/main.ts
@@ -5,13 +5,10 @@ import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
-    <div class="card">
-      <button type="button">Begin</button>
+    <div class="card" id="buttonholder">
       <button id="counter" type="button"></button>
     </div>
     <p class="read-the-docs">
-    next: add vr button
-    next: add begin button
     next: add vr hand
     </p>
   </div>
@@ -21,13 +18,15 @@ setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth*0.666 / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
-document.body.appendChild( VRButton.createButton( renderer ) );
-renderer.xr.enabled = true;
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( window.innerWidth*0.666, window.innerHeight );
+
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( VRButton.createButton( renderer ) );
+
+renderer.xr.enabled = true;
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
@@ -36,19 +35,21 @@ scene.add( cube );
 
 camera.position.z = 5;
 
-function animate() {
-	requestAnimationFrame( animate );
-
+function step() {
 	cube.rotation.x += 0.01;
 	cube.rotation.y += 0.01;
+}
 
+function animateOnPage() {
+	requestAnimationFrame( animateOnPage );
+    step()
 	renderer.render( scene, camera );
 }
 
-renderer.setAnimationLoop(function () {
+renderer.setAnimationLoop( function () {
+    step()
+	renderer.render( scene, camera );
 
-    renderer.render( scene, camera );
+} );
 
-});
-
-animate();
+animateOnPage();
