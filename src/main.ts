7cbb8ecd18aa50a6e3d42e43f20a4f23d39ce111
Ethan Muller <hi@emu.media>
2023-06-07 15:43:18 -0400

    add passthrough

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,16 @@
 import './style.css'
-import { setupCounter } from './counter.ts'
 import * as THREE from 'three';
-import { VRButton } from 'three/addons/webxr/VRButton.js';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
     <div class="card" id="buttonholder">
-      <button id="counter" type="button"></button>
     </div>
     <ul>
-        <li>next: better enter button</li>
         <li>next: add vr hand</li>
     </ul>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
 
 
 const scene = new THREE.Scene();
@@ -25,12 +20,11 @@ const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth*0.666, window.innerHeight );
 
 document.body.appendChild( renderer.domElement );
-document.body.appendChild( VRButton.createButton( renderer ) );
 
 renderer.xr.enabled = true;
 
 const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+const material = new THREE.MeshNormalMaterial();
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
@@ -68,3 +62,50 @@ renderer.xr.addEventListener('sessionend', function () {
     scene.remove(cameraGroup);
     cameraGroup.remove(camera);
 });
+
+
+// Function to handle VR mode
+function enterVR() {
+  // Check if the browser supports WebXR
+  if ('xr' in navigator) {
+
+    // Request VR session
+    navigator.xr
+      .requestSession('immersive-ar', { optionalFeatures: ['local-floor'] })
+      .then((session) => {
+        // Set the session to the renderer
+        renderer.xr.setSession(session);
+
+        // Render the scene in VR mode
+        function renderLoop() {
+          // Render your 3D scene here
+
+          // Submit the frame to the VR display
+          session.requestAnimationFrame(renderLoop);
+        }
+
+        // Start the render loop
+        session.requestAnimationFrame(renderLoop);
+      })
+      .catch((error) => {
+        console.error('Failed to enter VR session:', error);
+      });
+  } else {
+    console.error('WebXR is not supported in this browser.');
+  }
+}
+
+if ('xr' in navigator) {
+    // Create the VR button and add event listener
+    const vrButton = document.createElement('button');
+    vrButton.textContent = 'Enter VR';
+    vrButton.id = 'vr-button';
+    vrButton.addEventListener('click', enterVR);
+    document.querySelector('#buttonholder')?.appendChild(vrButton);
+} else {
+    // Create the VR button and add event listener
+    const msg = document.createElement('div');
+    msg.textContent = 'XR not supported';
+    msg.id = 'msg';
+    document.querySelector('#buttonholder')?.appendChild(msg);
+}
